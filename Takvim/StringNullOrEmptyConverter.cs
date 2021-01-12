95261db5d34707e29e4cd41a67eeeef1cc4046c8
goksenpasli <gpasli@gmail.com>
2021-01-12 19:55:19 +0300

    Fix Animation

---
+++ Takvim/StringNullOrEmptyConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Takvim
+{
+
+    public class StringNullOrEmptyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => string.IsNullOrWhiteSpace((string)value);
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}
