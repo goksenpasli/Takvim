3e60d7c3ca69dec03617d9f8d2d4a4e0e2ac6da7
goksenpasli <gpasli@gmail.com>
2021-01-12 21:02:52 +0300

    Active Month Color

--- Takvim/ŞuAnkiAyColorConverter.cs
+++ Takvim/ŞuAnkiAyColorConverter.cs
@@ -6,9 +6,9 @@ using System.Windows.Media;
 namespace Takvim
 {
 
-    public class StringNullOrEmptyConverter : IValueConverter
+    public class ŞuAnkiAyColorConverter : IValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => string.IsNullOrWhiteSpace((string)value);
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => value is string ay ? ay == DateTime.Now.ToString("MMMM") ? Brushes.Green : Brushes.Red : Brushes.Green;
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
     }
