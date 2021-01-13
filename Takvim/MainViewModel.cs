806bf791e3f4954437d67b2ed2b69e69ca8366f7
goksenpasli <gpasli@gmail.com>
2021-01-13 21:16:13 +0300

    Rounded Image

--- Takvim/MainViewModel.cs
+++ Takvim/MainViewModel.cs
@@ -2,11 +2,11 @@
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.IO;
+using System.Linq;
 using System.Windows;
 using System.Windows.Data;
 using System.Windows.Input;
 using System.Xml;
-using System.Linq;
 namespace Takvim
 {
     public class MainViewModel : InpcBase
