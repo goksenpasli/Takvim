d1587d7f71f57fe85231b672368c155482825e37
goksenpasli <gpasli@gmail.com>
2021-01-20 21:58:48 +0300

    Added Listbox Passed Day Visibility Converter

---
+++ Takvim/ListBoxDatePassedVisibilityConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Xml;
+
+namespace Takvim
+{
+    public class ListBoxDatePassedVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is XmlNode xmlNode)
+            {
+                if (DateTime.Parse(xmlNode["Gun"].InnerText) < DateTime.Today)
+                {
+                    return Visibility.Collapsed;
+                }
+                return Visibility.Visible;
+            }
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}
--- Takvim/SelectedDateToDataListConverter.cs
+++ Takvim/SelectedDateToDataListConverter.cs
@@ -13,5 +13,5 @@ namespace Takvim
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => value is Data data ? ((MainViewModel.xmlDataProvider?.Data as ICollection<XmlNode>)?.Where(z => DateTime.Parse(z["Gun"].InnerText) == data.TamTarih)) : null;
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
-    }
+    }   
 }
