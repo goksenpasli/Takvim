b62170b5a78f5e61e5ad27faba0618f9f11c354a
goksenpasli <gpasli@gmail.com>
2021-01-20 21:06:15 +0300

    Fixed Tabstops

---
+++ Takvim/SelectedDateToDataListConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+using System.Xml;
+
+namespace Takvim
+{
+
+    public class SelectedDateToDataListConverter : IValueConverter
+    {
+        private readonly ICollection<XmlNode> xmlNode;
+
+        public SelectedDateToDataListConverter() => xmlNode = MainViewModel.xmlDataProvider?.Data as ICollection<XmlNode>;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => value is Data data ? (xmlNode?.Where(z => DateTime.Parse(z["Gun"].InnerText) == data.TamTarih).Select(z => z["Aciklama"])) : null;
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}
