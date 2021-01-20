c65de6b26746f7f3cd1a068765001823a26dc13d
goksenpasli <gpasli@gmail.com>
2021-01-20 21:17:57 +0300

    Fixed Converter

--- Takvim/SelectedDateToDataListConverter.cs
+++ Takvim/SelectedDateToDataListConverter.cs
@@ -10,11 +10,7 @@ namespace Takvim
 
     public class SelectedDateToDataListConverter : IValueConverter
     {
-        private readonly ICollection<XmlNode> xmlNode;
-
-        public SelectedDateToDataListConverter() => xmlNode = MainViewModel.xmlDataProvider?.Data as ICollection<XmlNode>;
-
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => value is Data data ? (xmlNode?.Where(z => DateTime.Parse(z["Gun"].InnerText) == data.TamTarih).Select(z => z["Aciklama"])) : null;
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => value is Data data ? ((MainViewModel.xmlDataProvider?.Data as ICollection<XmlNode>)?.Where(z => DateTime.Parse(z["Gun"].InnerText) == data.TamTarih).Select(z => z["Aciklama"])) : null;
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
     }
