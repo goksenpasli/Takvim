65694b0daca48637d50ea0f9f6fe232469ef6874
goksenpasli <gpasli@gmail.com>
2021-01-23 12:07:35 +0300

    Added Ramadan Dates

---
+++ Takvim/RamadanDateToVisibilityConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Takvim
+{
+    public class RamadanDateToVisibilityConverter : IValueConverter
+    {
+        private readonly HijriCalendar hijriCalendar;
+
+        public RamadanDateToVisibilityConverter()
+        {
+            hijriCalendar = new HijriCalendar();
+        }
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => (value is DateTime date && date > hijriCalendar.MinSupportedDateTime) ? (hijriCalendar.GetMonth(date) == 9) ? Visibility.Visible : Visibility.Collapsed : Visibility.Collapsed;
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}
