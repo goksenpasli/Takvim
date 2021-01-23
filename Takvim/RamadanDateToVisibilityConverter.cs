5ce7533cc39ba32f3d913bc62c6a76f3b9b25de4
goksenpasli <gpasli@gmail.com>
2021-01-23 16:19:12 +0300

    Added Ramadan Holiday

--- Takvim/RamadanDateToVisibilityConverter.cs
+++ Takvim/RamadanDateToVisibilityConverter.cs
@@ -7,15 +7,22 @@ namespace Takvim
 {
     public class RamadanDateToVisibilityConverter : IValueConverter
     {
-        private readonly HijriCalendar hijriCalendar;
+        public HijriCalendar HijriCalendar { get; }
 
         public RamadanDateToVisibilityConverter()
         {
-            hijriCalendar = new HijriCalendar();
-            hijriCalendar.HijriAdjustment--;
+            HijriCalendar = new HijriCalendar();
+            HijriCalendar.HijriAdjustment--;
         }
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => (value is DateTime date && date > hijriCalendar.MinSupportedDateTime) ? (hijriCalendar.GetMonth(date) == 9) ? Visibility.Visible : Visibility.Collapsed : Visibility.Collapsed;
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => (value is DateTime date && date > HijriCalendar.MinSupportedDateTime) ? (HijriCalendar.GetMonth(date) == 9) ? Visibility.Visible : Visibility.Collapsed : Visibility.Collapsed;
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+
+    public class RamadanDateToHolidayConverter : RamadanDateToVisibilityConverter, IValueConverter
+    {
+        public new object Convert(object value, Type targetType, object parameter, CultureInfo culture) => value is DateTime date && date > HijriCalendar.MinSupportedDateTime && HijriCalendar.GetMonth(date) == 10 && (HijriCalendar.GetDayOfMonth(date) == 1 || HijriCalendar.GetDayOfMonth(date) == 2 || HijriCalendar.GetDayOfMonth(date) == 3);
+
+        public new object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
     }
 }
