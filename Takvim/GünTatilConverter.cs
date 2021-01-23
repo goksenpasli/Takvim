a6c7c5a42172690000a36ba3852ca6170672de63
goksenpasli <gpasli@gmail.com>
2021-01-23 20:39:24 +0300

    Optimize Holiday Converter

--- Takvim/GünTatilConverter.cs
+++ Takvim/GünTatilConverter.cs
@@ -8,20 +8,35 @@ namespace Takvim
 {
     public class GünTatilConverter : IValueConverter
     {
-        public List<Tuple<short, short, string>> Tatiller { get; } = new List<Tuple<short, short, string>>();
+        public List<Tuple<short, short, string, bool>> Tatiller { get; } = new List<Tuple<short, short, string, bool>>();
 
         public GünTatilConverter()
         {
-            Tatiller.Add(new Tuple<short, short, string>(1, 1, "Yılbaşı"));
-            Tatiller.Add(new Tuple<short, short, string>(4, 23, "Ulusal Egemenlik ve Çocuk Bayramı"));
-            Tatiller.Add(new Tuple<short, short, string>(5, 1, "Emek ve Dayanışma Günü"));
-            Tatiller.Add(new Tuple<short, short, string>(5, 19, "Atatürk'ü Anma, Gençlik ve Spor Bayramı"));
-            Tatiller.Add(new Tuple<short, short, string>(7, 15, "Demokrasi ve Millî Birlik Günü"));
-            Tatiller.Add(new Tuple<short, short, string>(8, 30, "Zafer Bayramı"));
-            Tatiller.Add(new Tuple<short, short, string>(10, 29, "Cumhuriyet Bayramı"));
+            Tatiller.Add(new Tuple<short, short, string, bool>(1, 1, "Yılbaşı", true));
+            Tatiller.Add(new Tuple<short, short, string, bool>(4, 23, "Ulusal Egemenlik ve Çocuk Bayramı", true));
+            Tatiller.Add(new Tuple<short, short, string, bool>(5, 1, "Emek ve Dayanışma Günü", true));
+            Tatiller.Add(new Tuple<short, short, string, bool>(5, 19, "Atatürk'ü Anma, Gençlik ve Spor Bayramı", true));
+            Tatiller.Add(new Tuple<short, short, string, bool>(7, 15, "Demokrasi ve Millî Birlik Günü", true));
+            Tatiller.Add(new Tuple<short, short, string, bool>(8, 30, "Zafer Bayramı", true));
+            Tatiller.Add(new Tuple<short, short, string, bool>(10, 28, "Cumhuriyet Bayramı", false));
+            Tatiller.Add(new Tuple<short, short, string, bool>(10, 29, "Cumhuriyet Bayramı", true));
         }
 
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => value is Data data && Tatiller.Any(z => z.Item1 == data.TamTarih.Month && z.Item2 == data.TamTarih.Day);
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Data data)
+            {
+                if (Tatiller.Any(z => z.Item1 == data.TamTarih.Month && z.Item2 == data.TamTarih.Day && z.Item4))
+                {
+                    return true;
+                }
+                if (Tatiller.Any(z => z.Item1 == data.TamTarih.Month && z.Item2 == data.TamTarih.Day && !z.Item4))
+                {
+                    return null;
+                }
+            }
+            return false;
+        }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
     }
