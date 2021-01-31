dbddefb0f8084c9749cfdb36c08721e248d05d5f
goksenpasli <gpasli@gmail.com>
2021-01-31 10:47:43 +0300

    date passed gray color

---
+++ Takvim/DatePassedBooleanConverter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Takvim
+{
+    public class DatePassedBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => value is DateTime date && date < DateTime.Today;
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}
