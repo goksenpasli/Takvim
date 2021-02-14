266e29fae36767f1ea75ee115f8032299b70897b
goksenpasli <gpasli@gmail.com>
2021-02-14 17:42:46 +0300

    Scanner Control Moved  to Toggle Button

---
+++ Takvim/BoolInverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace Takvim
+{
+
+    public class BoolInverter : MarkupExtension, IValueConverter
+    {
+        public override object ProvideValue(IServiceProvider serviceProvider) => this;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => value is bool x ? !x : value;
+        
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => Convert(value, targetType, parameter, culture);
+    }
+}
