9606f4e5c1433dda3d66946ef8cd8e413b85eb9a
goksenpasli <gpasli@gmail.com>
2021-02-15 22:31:35 +0300

    Added Image Print Option

--- Takvim/ImageViewer.cs
+++ Takvim/ImageViewer.cs
@@ -1,10 +1,45 @@
-﻿namespace Takvim
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Takvim
 {
     public class ImageViewer : InpcBase
     {
+        public ICommand Yazdır { get; }
+
+        public ImageViewer()
+        {
+        }
+
+        public ImageViewer(ImageSource imageSource) : this()
+        {
+            Yazdır = new RelayCommand<object>(parameter =>
+            {
+                PrintDialog pd = new PrintDialog();
+                DrawingVisual dv = new DrawingVisual();
+                if (pd.ShowDialog() == true)
+                {
+                    using (DrawingContext dc = dv.RenderOpen())
+                    {
+                        BitmapSource imagesource = imageSource.Width > imageSource.Height
+                            ? TwainControl.Picture.Resize((BitmapSource)imageSource, pd.PrintableAreaHeight, pd.PrintableAreaWidth, 90, 300, 300)
+                            : TwainControl.Picture.Resize((BitmapSource)imageSource, pd.PrintableAreaWidth, pd.PrintableAreaHeight, 0, 300, 300);
+                        imagesource.Freeze();
+                        dc.DrawImage(imagesource, new Rect(0, 0, pd.PrintableAreaWidth, pd.PrintableAreaHeight));
+                    }
+                    pd.PrintVisual(dv, "");
+                    imageSource = null;
+                }
+            }, parameter => true);
+        }
+
         private double angle;
 
         private double zoom = 1;
+
         public double Angle
         {
             get => angle;
--- Takvim/Viewer.xaml.cs
+++ Takvim/Viewer.xaml.cs
@@ -1,23 +1,21 @@
 ﻿using System;
 using System.Globalization;
 using System.Windows;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
 namespace Takvim
 {
-
     public partial class Viewer : Window, IDisposable
     {
         private bool disposedValue;
 
-        public Viewer()
-        {
-            InitializeComponent();
-            DataContext = new ImageViewer();
-        }
+        public Viewer() => InitializeComponent();
 
-        public Viewer(string file) : this() => Img.Source = (BitmapSource)new Base64ImageConverter().Convert(file, null, null, CultureInfo.CurrentCulture);
+        public Viewer(string file) : this()
+        {
+            Img.Source = (BitmapSource)new Base64ImageConverter().Convert(file, null, null, CultureInfo.CurrentCulture);
+            DataContext = new ImageViewer(Img.Source);
+        }
 
         public void Dispose()
         {
@@ -39,5 +37,6 @@ namespace Takvim
         }
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e) => Dispose(true);
+
     }
 }
