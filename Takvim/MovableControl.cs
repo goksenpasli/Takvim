cef7540042d0f1540a6d95bf03ff86156e15e218
goksenpasli <gpasli@gmail.com>
2021-02-23 19:48:42 +0300

    Added Drag Mouse Cursor

--- Takvim/MovableControl.cs
+++ Takvim/MovableControl.cs
@@ -2,17 +2,18 @@
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Input;
+using System.Windows.Media;
 using System.Xml;
 
 namespace Takvim
 {
-    public class MovableControl:DependencyObject
+    public class MovableControl : DependencyObject
     {
         public static bool GetMoveData(DependencyObject obj) => (bool)obj.GetValue(MoveDataProperty);
 
         public static void SetMoveData(DependencyObject obj, bool value) => obj.SetValue(MoveDataProperty, value);
 
-        public static readonly DependencyProperty MoveDataProperty = DependencyProperty.RegisterAttached("MoveData", typeof(bool), typeof(MovableControl), new PropertyMetadata(false,Changed));
+        public static readonly DependencyProperty MoveDataProperty = DependencyProperty.RegisterAttached("MoveData", typeof(bool), typeof(MovableControl), new PropertyMetadata(false, Changed));
 
         private static void Changed(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
@@ -20,6 +21,7 @@ namespace Takvim
             {
                 canvas.DragEnter += Canvas_DragEnter;
                 canvas.MouseMove += Canvas_MouseMove;
+                canvas.GiveFeedback += Canvas_GiveFeedback;
             }
             if (d is ListBox listBox)
             {
@@ -31,9 +33,28 @@ namespace Takvim
             }
         }
 
+        private static void Canvas_GiveFeedback(object sender, GiveFeedbackEventArgs e)
+        {
+            if (e.Effects == DragDropEffects.Move)
+            {
+                using Cursor customCursor = CursorHelper.CreateCursor(VisualTreeHelper.GetParent(e.Source as UIElement) as UIElement);
+                if (customCursor != null)
+                {
+                    e.UseDefaultCursors = false;
+                    Mouse.SetCursor(customCursor);
+                }
+            }
+            else
+            {
+                e.UseDefaultCursors = true;
+            }
+
+            e.Handled = true;
+        }
+
         private static void Button_DragEnter(object sender, DragEventArgs e)
         {
-            var dc = (sender as Button)?.DataContext as MainViewModel;
+            MainViewModel dc = (sender as Button)?.DataContext as MainViewModel;
             switch ((sender as Button)?.Tag.ToString())
             {
                 case "Ayİleri":
@@ -49,8 +70,8 @@ namespace Takvim
         {
             if (e.Data.GetDataPresent("data"))
             {
-                var dc = (e.Source as ListBox)?.DataContext as Data;
-                var dropdata = e.Data.GetData("data") as XmlNode;
+                Data dc = (e.Source as ListBox)?.DataContext as Data;
+                XmlNode dropdata = e.Data.GetData("data") as XmlNode;
                 dropdata["Gun"].InnerText = dc.TamTarih.ToString("o");
                 MainViewModel.xmlDataProvider.Document.Save(MainViewModel.xmlpath);
                 CollectionViewSource.GetDefaultView((Application.Current.MainWindow.DataContext as MainViewModel)?.AyGünler).Refresh();
@@ -62,7 +83,7 @@ namespace Takvim
         {
             if (e.LeftButton == MouseButtonState.Pressed)
             {
-                var dc = (sender as Canvas)?.DataContext as XmlNode;
+                XmlNode dc = (sender as Canvas)?.DataContext as XmlNode;
                 DragDrop.DoDragDrop(sender as Canvas, new DataObject("data", dc), DragDropEffects.Move);
             }
         }
