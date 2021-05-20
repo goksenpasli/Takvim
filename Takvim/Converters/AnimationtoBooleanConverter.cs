6792a8effd9873c66bbab04fcda14a140e2dc4c6
goksenpasli <gpasli@gmail.com>
2021-05-20 20:01:50 +0300

    Last Commit Finished

--- Takvim/Converters/AnimationtoBooleanConverter.cs
+++ Takvim/Converters/AnimationtoBooleanConverter.cs
@@ -1,16 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
 using System.Windows.Data;
-using System.Xml;
 
 namespace Takvim
 {
-    public class AnimationtoBooleanConverter : IValueConverter
+    public class AnimationtoBooleanConverter : IMultiValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => value is DateTime dateTime && MainViewModel.xmlDataProvider?.Data is ICollection<XmlNode> xmlNode && xmlNode.Count(z => DateTime.Parse(z["Gun"]?.InnerText) == dateTime) > 0;
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture) => values[0] is DateTime dateTime && values[1] is Data data && dateTime == data.TamTarih && data.VeriSayısı > 0;
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) => throw new NotImplementedException();
     }
 }
