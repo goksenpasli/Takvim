ff6b2a06f0529b8af58b88f7687fc3ac1d60e94b
goksenpasli <gpasli@gmail.com>
2021-05-26 22:25:12 +0300

    Last Commit Finished

--- Takvim/Controls/NumericUpDownControl.cs
+++ Takvim/Controls/NumericUpDownControl.cs
@@ -5,6 +5,14 @@ namespace Takvim
 {
     public class NumericUpDownControl : ScrollBar
     {
+        public bool IsReadOnly
+        {
+            get => (bool)GetValue(IsReadOnlyProperty);
+            set => SetValue(IsReadOnlyProperty, value);
+        }
+
+        public static readonly DependencyProperty IsReadOnlyProperty = DependencyProperty.Register("IsReadOnly", typeof(bool), typeof(NumericUpDownControl), new PropertyMetadata(false));
+
         static NumericUpDownControl() => DefaultStyleKeyProperty.OverrideMetadata(typeof(NumericUpDownControl), new FrameworkPropertyMetadata(typeof(NumericUpDownControl)));
     }
 }
