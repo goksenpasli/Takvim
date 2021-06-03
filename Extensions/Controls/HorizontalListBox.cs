89d78417494846fa6c7a326cc8228136065919b9
goksenpasli <gpasli@gmail.com>
2021-06-03 21:52:21 +0300

    Last Commit Finished

---
+++ Extensions/Controls/HorizontalListBox.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Extensions
+{
+    public class HorizontalListBox : ListBox
+    {
+        static HorizontalListBox() => DefaultStyleKeyProperty.OverrideMetadata(typeof(HorizontalListBox), new FrameworkPropertyMetadata(typeof(HorizontalListBox)));
+    }
+}
